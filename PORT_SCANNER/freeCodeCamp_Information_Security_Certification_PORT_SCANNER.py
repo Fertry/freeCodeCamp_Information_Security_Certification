2a55294d000e4e301e5bbb171cdd9124a7b52ec2
Alejandro Fdez <alejandro@fertry.tech>
2020-08-19 13:58:32 +0200

    Port Scanner init

---
+++ PORT_SCANNER/freeCodeCamp_Information_Security_Certification_PORT_SCANNER.py
@@ -0,0 +1,9 @@
+# Python for Penetration Testing -- Information Security Certification from freeCodeCamp
+# https://github.com/Fertry/freeCodeCamp_Information_Security_Certification
+# This file is a PORT SCANNER
+
+import socket
+
+sck = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
