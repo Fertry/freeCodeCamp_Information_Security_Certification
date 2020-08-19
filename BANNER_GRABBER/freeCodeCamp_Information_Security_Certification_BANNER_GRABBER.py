edec4d1bce463d77febca1587e38a4f2b31dfbac
Alejandro Fdez <alejandro@fertry.tech>
2020-08-19 12:50:16 +0200

    Banner Grabber completed

--- BANNER_GRABBER/freeCodeCamp_Information_Security_Certification_BANNER_GRABBER.py
+++ BANNER_GRABBER/freeCodeCamp_Information_Security_Certification_BANNER_GRABBER.py
@@ -4,3 +4,28 @@
 
 import socket
 
+# Función banner
+def banner(ip, port):
+
+    # Declaramos un objeto de tipo 'socket'
+    sck = socket.socket()
+    # Iniciamos la conexión a la IP y puerto especificado
+    sck.connect((ip, int(port)))
+    # Declaramos un tiempo de espera de 20 segundos
+    sck.settimeout(20)
+    print(sck.recv(1024))
+
+# Función main
+def main():
+
+    print("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&")
+    ip = input("Please input an IP address: ")
+    print("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&")
+    port = input("Please input a PORT: ")
+    print("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&")
+    banner(ip, port)
+
+# Bucle principal
+if __name__ == "__main__":
+
+    main()
