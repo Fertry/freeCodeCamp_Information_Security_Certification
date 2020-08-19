a08162e21d4f28ea105feae60f56023a771490b1
Alejandro Fdez <alejandro@fertry.tech>
2020-08-19 17:53:54 +0200

    Update freeCodeCamp_Information_Security_Certification_PORT_SCANNER.py

--- PORT_SCANNER/freeCodeCamp_Information_Security_Certification_PORT_SCANNER.py
+++ PORT_SCANNER/freeCodeCamp_Information_Security_Certification_PORT_SCANNER.py
@@ -4,6 +4,33 @@
 
 import socket
 
+# Objeto de tipo 'socket'
 sck = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+# Función portScanner
+def portScanner(port, host):
 
+    if sck.connect_ex((host, port)):
+        print("The port " + str(port) + " is closed")
+    else:
+        print("The port " + str(port) + " is open")
+
+# Función principal
+def main():
+
+    # Tomamos un puerto para escanear
+    print("<------------------------------>")
+    prt = input("Please provide a port: ")
+    # Tomamos una IP para escanear
+    hst = input("Please provide an IP: ")
+    print("<------------------------------>")
+    print("You entered port: " + str(prt))    
+    print("You entered IP: " + str(hst))
+    print("<------------------------------>")
+    # Llamamos a portScanner()
+    portScanner(int(prt), hst)
+
+# Bucle principal
+if __name__ == "__main__":
+
+    main()
